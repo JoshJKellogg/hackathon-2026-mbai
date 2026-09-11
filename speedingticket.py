182d31f36f3d2eb227b6e5d132ea62af22eddb4f
JoshJKellogg <josh.jeon@kellogg.northwestern.edu>
2026-09-11 18:08:25 -0500

    Update speedingticket.py

--- speedingticket.py
+++ speedingticket.py
@@ -1,4 +1,4 @@
-
+# Josh Jeon and CJ (Christopher) Priedel
 # Write a program that is given two integers representing a speed limit and driving speed in miles per hour (mph) and outputs the traffic ticket amount.
 # Driving 10 mph *under* the speed limit (or slower) receives a  \$50 ticket. Driving 6 - 20 mph *over* the speed limit receives a \$75 ticket.
 # Driving 21 - 40 mph *over* the speed limit receives a \$150 ticket. Driving faster than 40 mph over the speed limit receives a \$300 ticket. Otherwise, no ticket is received
